78a667655e3034b2e2b7dac6dde9bffe5ac08d89
Kamal Osman <kamaltmo@gmail.com>
2017-12-10 16:56:15 +0000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () =>  (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to Kamal Osman's porfolio</h1>
+      </header>
+      <p className="App-intro">
+        As you can probably guess, this is a work in progress
+      </p>
+    </div>
+  );
+
+export default App;
